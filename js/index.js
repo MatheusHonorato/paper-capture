09ca0f9623859824e797bab7a6e344aa3214ced1
Matheus Honorato <matheuspaixaohonorato@gmail.com>
2020-06-19 23:41:36 -0300

    up

--- js/index.js
+++ js/index.js
@@ -109,3 +109,29 @@ document.onreadystatechange = function() {
           "body").style.visibility = "visible"; 
     } 
 }; 
+
+// scroll opacity
+let firstHeading = document.getElementById("first-heading");
+
+function isVisible(element) {
+    let elementBox = element.getBoundingClientRect();
+    let distanceFromTop = -200; 
+
+    if(elementBox.top - window.innerHeight < distanceFromTop) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function scanDocument() {
+    let sectionList = document.querySelectorAll('.hidden');
+    sectionList.forEach(function(section) {
+        if(isVisible(section)) {
+            section.classList.remove('hidden');
+        };
+    });
+}
+
+
+document.addEventListener("scroll", scanDocument);
